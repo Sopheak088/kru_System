332ef60e32ed4c25ee71d5c7945409fc89cc8321
Sopheak088 <videolesson2907@gmail.com>
2019-10-23 11:02:47 +0700

    tttt

---
+++ Project/Projects/School_System/Database/Phoan.sql
@@ -0,0 +1,5 @@
+Create Database SCHOOL On (Name = DBNOTICE, FileName = "E:\School_Project\SCHOOL\SCHOOL.mdf",
+Size = 5MB,MaxSize = Unlimited, FileGrowth = 2MB)
+Log On (Name = DBNOTICE_log, FileName = "E:\School_Project\SCHOOL\SCHOOL_log.ldf",
+Size = 4MB, MaxSize = Unlimited, FileGrowth =10%)
+Go
